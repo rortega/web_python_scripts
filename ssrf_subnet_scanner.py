0889d01859aac6225dc563f5e868e71ce91756a1
rortega <ortega.r.rafael@gmail.com>
2024-01-11 13:11:00 -0600

    Update ssrf_subnet_scanner.py

--- ssrf_subnet_scanner.py
+++ ssrf_subnet_scanner.py
@@ -8,7 +8,7 @@ parser.add_argument('-t','--target',help='host/ip to target', required=True)
 parser.add_argument('--timeout',help='timeout',required=False,default=3)
 
 parser.add_argument('-v','--verbose',help='enable verbose mode',action="store_true",default=False)
-#/bin/python3 /home/kali/Desktop/OSWE/ssrf_subnet_scanner.py -t http://apigateway:8000/files/import --timeout 2
+#/bin/python3 /home/kali/Desktop/OSWE/ssrf_subnet_scanner.py -t http://xxx:8000/files/import --timeout 2
 args = parser.parse_args()
 
 base_ip = "http://172.16.16.{four}"
